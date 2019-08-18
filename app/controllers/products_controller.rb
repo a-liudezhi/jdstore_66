d849cd0474f1d1817d4dfdd10d0fab07117677a8
liudezhi <a.liudezhi@gmail.com>
2019-08-18 14:42:11 +0800

    implement add_product_to_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,7 @@ class ProductsController < ApplicationController
 
       def add_to_cart
     @product = Product.find(params[:id])
-    redirect_to :back
+      redirect_back(fallback_location: root_path)
     flash[:notice] = "测试加入购物车"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_121352) do
+ActiveRecord::Schema.define(version: 2019_08_18_063536) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "product_id"
+    t.integer "quantity", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
