8d28ae3e31dfcfae0ded00a52d9566d34258a29f
liudezhi <a.liudezhi@gmail.com>
2019-08-20 23:07:42 +0800

    local_time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require local_time
+//  require local_time
 //= require jquery
 //= require bootstrap-sprockets
 //= require rails-ujs
